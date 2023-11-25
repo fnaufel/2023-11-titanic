ff2e17f73dcc2cddb7e6e913171b889a41dd5b45
Fernando Naufel <fnaufel@gmail.com>
2023-11-25 18:19:02 -0300

    Redo EDA, split recipes

---
+++ R/functions.R
@@ -0,0 +1,47 @@
+
+# Separar componentes do nome ---------------------------------------------
+
+extrair_familia <- function(s) {
+  str_split_i(s, ',[ ]*', 1)
+}
+
+extrair_titulo <- function(s) {
+  s <- str_split_i(s, ',[ ]*', 2)
+  str_split_i(s, stringr::fixed('.'), 1)
+}
+
+extrair_nome <- function(s) {
+  s <- str_split_i(s, ',[ ]*', 2)
+  s <- str_split_i(s, stringr::fixed('.'), 2)
+  str_trim(s)
+}
+
+
+# Receita para logo depois da leitura -------------------------------------
+
+receita_pos_leitura <- function(df) {
+  
+  recipe(df) %>% 
+    step_mutate(survived = survived == 1) %>% 
+    step_mutate(pclass = fct_rev(ordered(pclass))) %>% 
+    step_mutate(
+      family = extrair_familia(name),
+      title = factor(extrair_titulo(name))
+    ) %>% 
+    step_mutate(
+      name = extrair_nome(name),
+      role = 'id'
+    ) %>% 
+    step_mutate(
+      sex = as_factor(sex) %>% 
+        fct_relabel(
+          ~case_when(
+            . == 'male' ~ 'M',
+            . == 'female' ~ 'F',
+          )
+        )
+    ) %>% 
+    step_mutate(embarked = as_factor(embarked))
+  
+}
+
